f50f17b622410ad736081cc4c06fb3c041eae53d
GirlGamerNL <57950826+GirlGamerNL@users.noreply.github.com>
2021-02-10 14:08:03 +0100

    SnelleCharacterCustomisation_Celine

--- Assets/Developers/Celine/Scripts/Character/SpawnPlayer.cs
+++ Assets/Developers/Celine/Scripts/Character/SpawnPlayer.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class SpawnPlayer : MonoBehaviour
 {
-   
+    [Header("PlayerOptions")]
+    public GameObject player1;
+    public GameObject player2;
+
+
+    public void ChoosePlayer1()
+    {
+        player1.SetActive(true);
+        player2.SetActive(false);
+    }
+
+    public void ChoosePlayer2()
+    {
+        player2.SetActive(true);
+        player1.SetActive(false);
+    }
+
 }
--- Assets/Developers/Celine/Scripts/UI/StartScreen.cs
+++ Assets/Developers/Celine/Scripts/UI/StartScreen.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class StartScreen : MonoBehaviour
 {
-    public GameObject player;
     public GameObject playerSpawnPosition;
     public GameObject StartPanel;
+
+    [Header("PlayerOptions")]
+    public GameObject player1;
+    public GameObject player2;
+    public bool player1Chosen;
+    public bool player2Chosen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,13 +27,45 @@ public class StartScreen : MonoBehaviour
 
     public void StartGame()
     {
+
+        if (player1Chosen == true)
+        {
+            Instantiate(player1, playerSpawnPosition.transform.position, Quaternion.identity);
+        }
+        else if (player2Chosen == true)
+        {
+            Instantiate(player2, playerSpawnPosition.transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogError("No player chosen");
+        }
+
         StartPanel.SetActive(false);
         Time.timeScale = 1f;
-        Instantiate(player, playerSpawnPosition.transform.position, Quaternion.identity);
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
+
+  
+
+    public void ChoosePlayer1()
+    {
+        player1.SetActive(true);
+        player2.SetActive(false);
+        player1Chosen = true;
+        player2Chosen = false;
+    }
+
+    public void ChoosePlayer2()
+    {
+        player2.SetActive(true);
+        player1.SetActive(false);
+        player1Chosen = false;
+        player2Chosen = true;
+    }
+
 }
