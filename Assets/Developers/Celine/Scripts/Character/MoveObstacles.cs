2a2c929f7ef923df9690b916e144a0e9b9774d52
GirlGamerNL <57950826+GirlGamerNL@users.noreply.github.com>
2021-02-17 14:03:19 +0100

    ObstaclesFixed

--- Assets/Developers/Celine/Scripts/Character/MoveObstacles.cs
+++ Assets/Developers/Celine/Scripts/Character/MoveObstacles.cs
@@ -4,33 +4,32 @@ using UnityEngine;
 
 public class MoveObstacles : MonoBehaviour
 {
-
-    public int speed;
     private Rigidbody2D rb2d;
-    // Start is called before the first frame update
+    public Vector2 speed;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         //Spawn/speed changes 
         if (GameManager_Celine.instance.Waves <= 4)
         {
             GameManager_Celine.instance.timeBetweenSpawns = 5f;
-            speed = 2 + Random.Range(2, 10);
         }
         else if (GameManager_Celine.instance.Waves > 4)
         {
+            GameManager_Celine.instance.timeBetweenSpawns = 3f;
+        }
+        else if (GameManager_Celine.instance.Waves > 6)
+        {
             GameManager_Celine.instance.timeBetweenSpawns = 1f;
-            speed = 4 + Random.Range(2, 10);
         }
        
         //Move the obstacles to the right
-        Vector3 move = new Vector3(-4f, 0);
-        transform.position += move * speed * Time.deltaTime;
+        rb2d.velocity = speed;
 
     }
 
